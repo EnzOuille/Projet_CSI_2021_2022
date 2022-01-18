117b115ee3f418a4f6cf4e3350e762b92880bb03
Glarell <pilou.tondon@hotmail.fr>
2022-01-18 15:27:08 +0100

    push

--- CSI/src/main/java/com/csi/CSI/repositories/AbonneRepo.java
+++ CSI/src/main/java/com/csi/CSI/repositories/AbonneRepo.java
@@ -9,5 +9,6 @@ public interface AbonneRepo extends CrudRepository<Abonne, String> {
     @Query(value="select * from abonne where abn_pseudo= ?1 and abn_mdp = ?2", nativeQuery = true)
     Abonne getAbonneByLoginMdp(String pseudo, String mdp);
 
+    @Query(value="SELECT * from abonne where abn_id= ?1 ");
 }
 
