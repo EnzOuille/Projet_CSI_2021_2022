158157711bef98c72884b8385db77631b4f23f38
EnzOuille <enzo_cisternino@hotmail.fr>
2022-01-23 16:46:57 +0100

    Travail Enzo

--- CSI/src/main/java/com/csi/CSI/controller/HomeController.java
+++ CSI/src/main/java/com/csi/CSI/controller/HomeController.java
@@ -52,6 +52,48 @@ public class HomeController {
     @GetMapping("/news")
     public String list_news_internaute(Model model, HttpServletRequest request, HttpSession session) {
         String abn_id = "" ;
+        List<Domaine> cats = domRepo.findAllActive();
+        HashMap<String, List<News>> news_list_3 = new HashMap<>();
+        HashMap<String, List<News>> news_list = new HashMap<>();
+        for (Domaine cat : cats) {
+            List<News> last3 = newsRepo.findNewsByCategoryLast3((int) cat.getDom_id());
+            news_list_3.put(cat.getDom_nom(), last3);
+            List<News> news = newsRepo.findNewsByCategory((int) cat.getDom_id());
+            news_list.put(cat.getDom_nom(), news);
+        }
+        model.addAttribute("news_categories", news_list);
+        model.addAttribute("last3_categories", news_list_3);
+        List<MotCle> keywords = keyRepo.findAll();
+        HashMap<String, List<News>> news_keywords = new HashMap<>();
+        for (MotCle key : keywords) {
+            List<News> news_key = newsRepo.findNewsByKeyword((int) key.getMtc_id());
+            news_keywords.put(key.getMtc_nom(), news_key);
+        }
+        model.addAttribute("news_keywords", news_keywords);
+        List<News> news = newsRepo.findAllActiveNews();
+        List<NewsDisplay> disp_news = new ArrayList<>();
+        for (News temp_new : news) {
+            Abonne abonne1 = abnRepo.getAbonneById((int) temp_new.getNew_abn_id());
+            MotCle mtc1 = keyRepo.getMotCleById(temp_new.getNew_mtc_1());
+            MotCle mtc2 = keyRepo.getMotCleById(temp_new.getNew_mtc_2());
+            MotCle mtc3 = keyRepo.getMotCleById(temp_new.getNew_mtc_3());
+            Domaine dom = domRepo.getDomaineById((int) temp_new.getNew_dom_id());
+            NewsDisplay newDisp = new NewsDisplay(temp_new, abonne1.getAbn_nom() + " " + abonne1.getAbn_prenom(), mtc1.getMtc_nom(), mtc2.getMtc_nom(), mtc3.getMtc_nom(), dom.getDom_nom());
+            disp_news.add(newDisp);
+        }
+        model.addAttribute("news", disp_news);
+        List<ArchivageNews> archive_news = archRepo.findAll();
+        List<NewsDisplay> list_archive_news = new ArrayList<>();
+        for (ArchivageNews temp_new : archive_news){
+            Abonne abonne1 = abnRepo.getAbonneById((int) temp_new.getArc_abn_id());
+            MotCle mtc1 = keyRepo.getMotCleById(temp_new.getArc_mtc_1());
+            MotCle mtc2 = keyRepo.getMotCleById(temp_new.getArc_mtc_2());
+            MotCle mtc3 = keyRepo.getMotCleById(temp_new.getArc_mtc_3());
+            Domaine dom = domRepo.getDomaineById((int) temp_new.getArc_dom_id());
+            NewsDisplay newDisp = new NewsDisplay(temp_new, abonne1.getAbn_nom() + " " + abonne1.getAbn_prenom(), mtc1.getMtc_nom(), mtc2.getMtc_nom(), mtc3.getMtc_nom(), dom.getDom_nom());
+            list_archive_news.add(newDisp);
+        }
+        model.addAttribute("news_archive", list_archive_news);
         try{
             abn_id = session.getAttribute("abn_id").toString();
         }catch (Exception e){
@@ -65,51 +107,9 @@ public class HomeController {
                 model.addAttribute("type", "confiance");
             } else {
                 model.addAttribute("type", "abonne");
-                List<News> news = newsRepo.findAllActiveNews();
-                List<NewsDisplay> disp_news = new ArrayList<>();
-                for (News temp_new : news) {
-                    Abonne abonne1 = abnRepo.getAbonneById((int) temp_new.getNew_abn_id());
-                    MotCle mtc1 = keyRepo.getMotCleById(temp_new.getNew_mtc_1());
-                    MotCle mtc2 = keyRepo.getMotCleById(temp_new.getNew_mtc_2());
-                    MotCle mtc3 = keyRepo.getMotCleById(temp_new.getNew_mtc_3());
-                    Domaine dom = domRepo.getDomaineById((int) temp_new.getNew_dom_id());
-                    NewsDisplay newDisp = new NewsDisplay(temp_new, abonne1.getAbn_nom() + " " + abonne1.getAbn_prenom(), mtc1.getMtc_nom(), mtc2.getMtc_nom(), mtc3.getMtc_nom(), dom.getDom_nom());
-                    disp_news.add(newDisp);
-                }
-                model.addAttribute("news", disp_news);
-                List<ArchivageNews> archive_news = archRepo.findAll();
-                List<NewsDisplay> list_archive_news = new ArrayList<>();
-                for (ArchivageNews temp_new : archive_news){
-                    Abonne abonne1 = abnRepo.getAbonneById((int) temp_new.getArc_abn_id());
-                    MotCle mtc1 = keyRepo.getMotCleById(temp_new.getArc_mtc_1());
-                    MotCle mtc2 = keyRepo.getMotCleById(temp_new.getArc_mtc_2());
-                    MotCle mtc3 = keyRepo.getMotCleById(temp_new.getArc_mtc_3());
-                    Domaine dom = domRepo.getDomaineById((int) temp_new.getArc_dom_id());
-                    NewsDisplay newDisp = new NewsDisplay(temp_new, abonne1.getAbn_nom() + " " + abonne1.getAbn_prenom(), mtc1.getMtc_nom(), mtc2.getMtc_nom(), mtc3.getMtc_nom(), dom.getDom_nom());
-                    list_archive_news.add(newDisp);
-                }
-                model.addAttribute("news_archive", list_archive_news);
             }
         } else {
             model.addAttribute("type", "internaute");
-            List<Domaine> cats = domRepo.findAllActive();
-            HashMap<String, List<News>> news_list_3 = new HashMap<>();
-            HashMap<String, List<News>> news_list = new HashMap<>();
-            for (Domaine cat : cats) {
-                List<News> last3 = newsRepo.findNewsByCategoryLast3((int) cat.getDom_id());
-                news_list_3.put(cat.getDom_nom(), last3);
-                List<News> news = newsRepo.findNewsByCategory((int) cat.getDom_id());
-                news_list.put(cat.getDom_nom(), news);
-            }
-            model.addAttribute("news_categories", news_list);
-            model.addAttribute("last3_categories", news_list_3);
-            List<MotCle> keywords = keyRepo.findAll();
-            HashMap<String, List<News>> news_keywords = new HashMap<>();
-            for (MotCle key : keywords) {
-                List<News> news_key = newsRepo.findNewsByKeyword((int) key.getMtc_id());
-                news_keywords.put(key.getMtc_nom(), news_key);
-            }
-            model.addAttribute("news_keywords", news_keywords);
         }
         return "consulting_news";
     }
