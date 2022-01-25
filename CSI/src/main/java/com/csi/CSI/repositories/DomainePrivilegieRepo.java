a2502e989438dfa131b1cb0f294f8368733a33dc
Alexandre Sousa <eoloc57@gmail.com>
2022-01-26 00:08:12 +0100

    Ajout de recherche par abonné et domaine

--- CSI/src/main/java/com/csi/CSI/repositories/DomainePrivilegieRepo.java
+++ CSI/src/main/java/com/csi/CSI/repositories/DomainePrivilegieRepo.java
@@ -11,4 +11,7 @@ public interface DomainePrivilegieRepo extends CrudRepository<DomainePrivilegie,
 
     @Query(value="SELECT * FROM domaineprivilegie WHERE dpl_abn_id = ?1", nativeQuery=true)
     List<DomainePrivilegie> findDomainesByAbonne(int abn_id);
+
+    @Query(value="SELECT * FROM domaineprivilegie WHERE dpl_abn_id = ?1 AND dpl_dom_id = ?2 LIMIT 1", nativeQuery=true)
+    DomainePrivilegie findDomaineByAbonneAndDomaine(long abn_id, long domain_id);
 }
