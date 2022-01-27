20892263e7819ae1bed9f364c2da1a0e520f99e3
CISTERNINO Enzo <enzo_cisternino@hotmail.fr>
2022-01-27 09:26:35 +0100

    Add Getter And Setter

--- CSI/src/main/java/com/csi/CSI/objets/DomainePrivilegie.java
+++ CSI/src/main/java/com/csi/CSI/objets/DomainePrivilegie.java
@@ -25,4 +25,20 @@ public class DomainePrivilegie implements Serializable {
         this.dpl_abn_id=dpl_abn_id;
         this.dpl_dom_id=dpl_dom_id;
     }
+
+    public long getDpl_abn_id() {
+        return dpl_abn_id;
+    }
+
+    public void setDpl_abn_id(long dpl_abn_id) {
+        this.dpl_abn_id = dpl_abn_id;
+    }
+
+    public long getDpl_dom_id() {
+        return dpl_dom_id;
+    }
+
+    public void setDpl_dom_id(long dpl_dom_id) {
+        this.dpl_dom_id = dpl_dom_id;
+    }
 }
